0b941bccf275c937de91112a85127db3dea8697e
Mojtaba Dashtinejad <mojtaba@gmail.com>
2017-03-13 14:40:47 +0330

    add 1999 numbers to test

--- hamming.spec.js
+++ hamming.spec.js
@@ -1,8 +1,8 @@
 const chai = require('chai')
 const expect = chai.expect
 const hamming = require('./hamming')
+const numbers = require('./numbers')
 
-const numbers = [1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27, 30, 32, 36, 40]
 
 describe('Sample Tests', () => {
   numbers.forEach((hammingNumber, index) => {

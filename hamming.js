0e03f83fe1e8caa63cc2ab8e919a2a78cf975968
Mojtaba Dashtinejad <mojtaba@gmail.com>
2017-03-13 14:33:27 +0330

    change the algorithm

--- hamming.js
+++ hamming.js
@@ -1,26 +1,25 @@
-function isHammingNumber(num) {
-  while (num % 5 === 0) num /= 5
-  while (num % 3 === 0) num /= 3
-  while (num % 2 === 0) num /= 2
-
-  return num === 1
-}
-
 function hamming(n) {
-  const succession = [1]
-  var length = succession.length
-  var candidate = 2
+  const seq = [1]
+  var i = 0
+  var x = 1
+  var i2 = -1
 
-  while (length < n) {
-    if (isHammingNumber(candidate)) {
-      succession[length] = candidate
-      length++
-    }
+  var i3 = -1
+  var i5 = -1
 
-    candidate++
+  var x2 = 2
+  var x3 = 3
+  var x5 = 5
+
+  while (++i < n) {
+    seq[i] = x = (x2 <= x3 && x2 <= x5) ? x2 : (x3 <= x5) ? x3 : x5
+
+    while (x2 <= x) x2 = 2 * seq[++i2]
+    while (x3 <= x) x3 = 3 * seq[++i3]
+    while (x5 <= x) x5 = 5 * seq[++i5]
   }
-
-  return succession[n - 1]
+  
+  return seq[n - 1]
 }
 
 module.exports = hamming
